bc004148771d59cf013a2cbc277a0f060b64afa1
Chen Ming <chenming1@cffex.com.cn>
2018-07-04 10:14:05 +0800

    make changes

--- ch16-router/src/main.js
+++ ch16-router/src/main.js
@@ -7,7 +7,7 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
-  mode: 'history',
+  mode: 'history',//remove # tag form the context path.
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
--- ch16-router/src/routes.js
+++ ch16-router/src/routes.js
@@ -25,24 +25,25 @@ const UserDetail = resolve => {
 export const routes = [
     {
         path: '', name: 'home', components: {
-        default: Home,
-        'header-top': Header
-    }
+            default: Home,
+            'header-top': Header
+        }
     },
     {
         path: '/user', components: {
         default: User,
         'header-bottom': Header
-    }, children: [
-        {path: '', component: UserStart},
-        {
-            path: ':id', component: UserDetail, beforeEnter: (to, from, next) => {
-            console.log('inside route setup');
-            next();
-        }
         },
-        {path: ':id/edit', component: UserEdit, name: 'userEdit'}
-    ]
+        children: [
+            {path: '', component: UserStart},
+            {
+                path: ':id', component: UserDetail, beforeEnter: (to, from, next) => {
+                console.log('inside route setup');
+                next();
+            }
+            },
+            {path: ':id/edit', component: UserEdit, name: 'userEdit'}
+        ]
     },
     {path: '/redirect-me', redirect: {name: 'home'}},
     {path: '*', redirect: '/'}
